fdb711fcacd9fa371e4304a39c0f221013969936
CJ McCaskill <cjmccaskill@gmail.com>
2021-09-20 07:53:20 -0500

    update database config

--- config/server.js
+++ config/server.js
@@ -2,8 +2,8 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   admin: {
-    // auth: {
-    //   secret: env('ADMIN_JWT_SECRET'),
-    // },
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
   },
 });
