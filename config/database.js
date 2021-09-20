3b0ca6e5cdb66f1c218f52faa9cf355869073a95
CJ McCaskill <cjmccaskill@gmail.com>
2021-09-19 22:38:25 -0500

    update database config

--- config/database.js
+++ config/database.js
@@ -1,12 +1,16 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'mongoose',
-      settings: {},
+      connector: "mongoose",
+      settings: {
+        host: env("DATABASE_HOST"),
+        port: env.int("DATABASE_PORT", 27017),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
+      },
       options: {},
     },
   },
 });
-
-
